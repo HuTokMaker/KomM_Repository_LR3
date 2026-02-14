056b01f4bda230fb44fc4864afdec97af6af264c
HuTokMaker <igorsemch135@gmail.com>
2026-02-14 10:16:07 +0300

    Funk1

--- KomM_LR3_project.cpp
+++ KomM_LR3_project.cpp
@@ -5,7 +5,19 @@ class Program
     //Разрабатывается Комляковом Максимом
     static double InputPositiveNumber()
     {
-
+        double a;
+        while (true)
+        {
+            Console.Write("Введите положительное число A: ");
+            if (double.TryParse(Console.ReadLine(), out a) && a > 0)
+            {
+                return a;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введите положительное число!");
+            }
+        }
     }
 
     //Разрабатывается Комляковом Максимом
