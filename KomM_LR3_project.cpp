da0d06e284741944c34fa09715d490ed9582a2bd
HuTokMaker <igorsemch135@gmail.com>
2026-02-14 10:04:52 +0300

    Create void main

---
+++ KomM_LR3_project.cpp
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    //Разрабатывается Комляковом Максимом
+    static double InputPositiveNumber()
+    {
+
+    }
+
+    //Разрабатывается Комляковом Максимом
+    static double CalculateArea(double a)
+    {
+
+    }
+
+    //Разрабатывается Комляковом Максимом
+    static double CalculatePerimeter(double a)
+    {
+
+    }
+}
